3063a99b97991ff6ee7df639f7f4bc895356e09c
David Alvarez <aalvag@live.com>
2022-02-28 01:48:27 -0300

    add bottom tab navigation

---
+++ src/navigation/bottomTabNavigator.js
@@ -0,0 +1,33 @@
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import HomeScreen from "../screens/HomeScreen";
+import { Entypo, FontAwesome, Foundation } from "@expo/vector-icons";
+
+const Tab = createBottomTabNavigator();
+
+const BottomTabNavigator = () => {
+  return (
+    <Tab.Navigator
+      initialRouteName="Home"
+      screenOptions={{
+        headerShown: false,
+        tabBarActiveTintColor: "#fff",
+        tabBarInactiveTintColor: "gray",
+        tabBarStyle: {
+          backgroundColor: "#181818",
+        },
+      }}
+    >
+      <Tab.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          tabBarIcon: ({ focused, color }) => (
+            <Entypo name="home" size={focused ? 30 : 25} color={color} />
+          ),
+        }}
+      />
+    </Tab.Navigator>
+  );
+};
+
+export default BottomTabNavigator;
--- src/navigation/index.js
+++ src/navigation/index.js
@@ -2,16 +2,19 @@ import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import HomeScreen from "../screens/HomeScreen";
 import CoinDetail from "../screens/CoinDetailScreen";
+import BottomTabNavigator from "./bottomTabNavigator";
 
 const Stack = createNativeStackNavigator();
 
 export default function Navigation() {
   return (
     <Stack.Navigator
+      initialRouteName="Root"
       screenOptions={{
         headerShown: false,
       }}
     >
+      <Stack.Screen name="Root" component={BottomTabNavigator} />
       <Stack.Screen name="Home" component={HomeScreen} />
       <Stack.Screen name="CoinDetail" component={CoinDetail} />
     </Stack.Navigator>
